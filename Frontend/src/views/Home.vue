db03cc44e42e1283a69bb1f24fc2c5567db31e53
ArURehman <ajeebareeb0147@gmail.com>
2023-05-25 00:06:54 +0500

    Added Vue Router and Home Page View

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
-  <RouterView/>
+    <h1 class="text-3xl font-bold">
+    Hello world!
+  </h1>
 </template>
 
 <script>
 
 export default{
-  name: 'App',
+  name: 'Home',
   setup(){
 
   }
