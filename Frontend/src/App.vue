c4d93d6ffc95d891f470a4da6b77121e17cd46bc
ArURehman <ajeebareeb0147@gmail.com>
2023-05-25 00:21:25 +0500

    Added and Tested Navar Component (Working)

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <Navbar/>
   <RouterView/>
 </template>
 
 <script>
+import Navbar from '../src/components/Navbar.vue'
 
 export default{
   name: 'App',
+  components: {Navbar},
   setup(){
 
   }
