3bc92f148b2e03613fd391d936ece44d512d7526
Areeb Ur Rehman <ajeebareeb0147@gmail.com>
2023-05-24 19:07:14 +0500

    Update dbConnect.js to remove exposed database credentials.

--- Backend/Database/dbConnect.js
+++ Backend/Database/dbConnect.js
@@ -2,7 +2,7 @@ import sql from 'mssql';
 
 const connectDB = async () => {
     try {
-        await sql.connect('Server=youtubeclonedb.c1m9gxojj8ba.ap-southeast-2.rds.amazonaws.com,1433;Database=YoutubeCloneDB;User Id=admin;Password=youtube621;Encrypt=false');
+        await sql.connect('Server=,1433;Database=YoutubeCloneDB;User Id=;Password=;Encrypt=false');
     } catch (err) {
         console.log(err)
     }
