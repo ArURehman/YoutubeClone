2e2d1771cd5b55631f480601029f628e34d84b34
ArURehman <ajeebareeb0147@gmail.com>
2023-05-25 19:50:55 +0500

    Added Font Awesome Package. Added Navbar (Mostly Complete)

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'Roboto': ['Roboto', 'sans-serif']
+    },
+    extend: {
+      spacing:{
+        '33': '8.25rem',
+        '34': '8.50rem',
+        '35': '8.75rem'
+      },
+      colors: {
+        'almostBlack': '#0f0f0f',
+        'almostGray': '#282828'
+      }
+    },
   },
   plugins: [],
 }
