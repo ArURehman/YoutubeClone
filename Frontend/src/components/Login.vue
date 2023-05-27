730807dede42bd371b6afbc8d1ec33a0d49dd4b0
ArURehman <ajeebareeb0147@gmail.com>
2023-05-27 21:18:46 +0500

    Added Sign Up page. Fixed minor bug involving the sign up and login page not closing completely once the backdrop was clicked.

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @click.self="emitCloseLogin" class="w-full h-full bg-searchBarGray absolute text-white">
+    <div @click.self="emitCloseLogin();emitOpenSignUp()" class="w-full h-full bg-searchBarGray absolute text-white">
         <div class="w-72 h-[22rem] bg-almostBlack m-auto relative z-30 top-36 rounded-xl p-4 flex-col">
             <div class="mx-[4.7rem] mt-5 flex gap-1">
                 <font-awesome-icon icon="fa-brands fa-youtube" size="2xl" style="color: #ff0000;" />
@@ -8,15 +8,15 @@
             <div class="p-[24px] mt-7">
                 <div class="pb-8 pt-2 pl-[10px] flex gap-2 font-Roboto text-sm font-light">
                     <font-awesome-icon class="mt-2" icon="fa-solid fa-user" size="md" style="color: #ffffff;" />
-                    <input class="bg-almostBlack border-b-2 border-b-white  font-light" type="text">
+                    <input placeholder="Email" class="bg-almostBlack border-b-2 border-b-white  font-light" type="text">
                 </div>
                 <div class="pb-8 pt-2 pl-[10px] flex gap-2 font-Roboto text-sm">
                     <font-awesome-icon class="mt-2" icon="fa-solid fa-lock" size="md" style="color: #ffffff;" />
-                    <input class="bg-almostBlack border-b-2 border-b-white font-light" type="text">
+                    <input placeholder="Password" class="bg-almostBlack border-b-2 border-b-white font-light" type="text">
                 </div>
                 <div class="mt-5 px-2 flex justify-around gap-2">
                     <button class="ease-in-out duration-300 border-2 border-[#ff0000] hover:font-medium rounded-xl px-4 py-2 hover:bg-[#ff0000]">Login</button>
-                    <button class="border-2 border-[#ff0000] duration-300 hover:font-medium rounded-xl px-3 py-2 hover:bg-[#ff0000] ease-in-out">Sign Up</button>
+                    <button @click="emitOpenSignUp" class="border-2 border-[#ff0000] duration-300 hover:font-medium rounded-xl px-3 py-2 hover:bg-[#ff0000] ease-in-out">Sign Up</button>
                 </div>
             </div>
         </div>
@@ -30,6 +30,9 @@ export default{
     methods: {
         emitCloseLogin(){
             this.$emit('closeLogin')
+        },
+        emitOpenSignUp(){
+            this.$emit('openSignUpFL')
         }
     }
 }
